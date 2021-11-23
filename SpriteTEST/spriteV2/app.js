d3779844ef8347230f78e3deaa339a32a2bb890d
Angélica Sofía Pagés Burgos <68571768+angelicapages@users.noreply.github.com>
2021-11-22 23:50:07 -0300

    codeando modales, carrito funcionamiento lógico listo

--- SpriteTEST/spriteV2/app.js
+++ SpriteTEST/spriteV2/app.js
@@ -9,6 +9,34 @@ const imagenMostrada8 = document.querySelector(".imagenMostradaCasco2")
 const colores = document.querySelectorAll(".coloresJs");
 const rodados = document.querySelectorAll(".rodado")
 const cards = document.querySelectorAll(".card")
+let id;
+let rodadoSelecionado;
+var totalPago = 0;
+const carrito = [];
+let botonesPrecio = document.querySelectorAll(".precio")
+const articulos = ["Specialized", "Bici2", "Bici3", "Bici4", "Bici5"];
+const promociones = ["PromoBasica", "PromoBiciteka", "PromoPremiun"]
+const valoresPromociones = [75000, 120000, 250000]
+let articuloSeleccionado;
+const carritoAcumuladora = document.querySelector("#carritoAcumuladora")
+const overlay = document.querySelector('#overlay');
+let acumuladoraSeleccionado;
+var pagar;
+var cuenta1;
+var vuelto;
+
+const abrirModal = () => {
+    overlay.classList.remove('hidden');
+    //body.classList.add('no-scroll')
+};
+
+const cerrarModal = () => {
+    overlay.classList.add('hidden');
+    for (let dialogo of ventanasDeDialogo) {
+        dialogo.classList.add('hidden');
+    }
+};
+
 const cambioDeImagen = (id) => {
     switch (id) {
         case "roja":
@@ -75,32 +103,94 @@ const cambioDeImagen = (id) => {
             imagenMostrada7.setAttribute("src", `./images/${id}.png`)
             break;
         case "casco4":
-                imagenMostrada8.setAttribute("src", `./images/${id}.png`)
-                break;
+            imagenMostrada8.setAttribute("src", `./images/${id}.png`)
+            break;
         case "casco5":
-                imagenMostrada8.setAttribute("src", `./images/${id}.png`)
-                break;
+            imagenMostrada8.setAttribute("src", `./images/${id}.png`)
+            break;
         case "casco6":
-                imagenMostrada8.setAttribute("src", `./images/${id}.png`)
-                break;
+            imagenMostrada8.setAttribute("src", `./images/${id}.png`)
+            break;
         default:
             break;
     }
 }
+
+
 colores.forEach(color => {
     color.addEventListener("click", () => {
-        let id = color.getAttribute("id")
+        id = color.getAttribute("id")
         cambioDeImagen(id)
     })
 })
+
 const eliminarActive = () => {
     rodados.forEach(rodado => {
         rodado.classList.remove("active")
+        rodadoSelecionado = ""
     })
 }
+
 rodados.forEach(rodado => {
     rodado.addEventListener("click", () => {
         eliminarActive()
         rodado.classList.add("active")
     })
 })
+
+function acumularCarrito() {
+    acumuladoraSeleccionado = carrito.length
+    carritoAcumuladora.innerHTML = `<p>${acumuladoraSeleccionado}</p>`
+    console.log(acumuladoraSeleccionado)
+}
+
+botonesPrecio.forEach((botonPrecio) =>
+    botonPrecio.onclick = () => {
+        totalPago += parseInt(botonPrecio.dataset.precio)
+        articuloSeleccionado = botonPrecio.dataset.producto
+        carrito.push(articuloSeleccionado)
+        console.log(carrito)
+        acumularCarrito()
+        eliminarActive()
+    }
+);
+
+// botonesAdquirirPromo.forEach((botonAdquirirPromo)=>
+// botonAdquirirPromo.onclick = () => {
+// totalPago += parseInt(botonAdquirirPromo.dataset.valor)
+// carrito.push(botonAdquirirPromo)    
+// console.log(carrito) 
+// }
+// );   
+//  
+
+
+
+function vaciarCarrito() {
+    totalPago = 0
+    carrito = [];
+}
+
+function carritoCompra() {
+    if (totalPago === 0) {
+        alert("compra algo raton")
+    } else {
+        alert("Compraste estos articulos: " + "\n" + carrito + " tenes que pagar $ " + totalPago)
+        pagar = parseInt(prompt("¿Con cuento va pagar ?"))
+        if (pagar > totalPago) {
+            vuelto = pagar - totalPago;
+            alert("Muchas gracias por su compra ;)\n Su vuelto es $ " + vuelto)
+            vaciarCarrito()
+        }
+        else if (totalPago > pagar) {
+            cuenta1 = (totalPago - pagar);
+            alert("Te faltan $" + cuenta1)
+            carritoCompra()
+        }
+        else {
+            alert("Muchas gracias por su compra ;)")
+            vaciarCarrito()
+        }
+    }
+    console.log(carrito)
+}
